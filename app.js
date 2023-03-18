2011e98b5de92141c94eea75ddd8ee90507264f3
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-03-18 18:34:01 -0500

    calculation result showed

--- app.js
+++ app.js
@@ -4,4 +4,4 @@
 let miles = 26.2;
 km = miles * 1.60934;
 roundedKm = km.toFixed(2);
-// console.log(`The distance of a marathon is 26.2 miles which equals ${roundedKm} kilometers.`);
+console.log(`The distance of a marathon is 26.2 miles which equals ${roundedKm} kilometers.`);
